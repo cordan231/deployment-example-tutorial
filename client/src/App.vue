3617c3b59af783118a4d822ae401a9d5f6cad628
cordan231 <142618197+cordan231@users.noreply.github.com>
2025-09-15 23:05:53 +0000

    Add Dockerfile and compose file for deployment

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const users = ref([])
+
+fetch('/users/').then(response => response.json())
+.then((response) => {
+  users.value = response
+}).catch((e) => {
+  console.error(e)
+})
+</script>
+
+<template>
+  <h1>List of Users</h1>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.username }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
